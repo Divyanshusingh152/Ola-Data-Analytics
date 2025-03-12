64fb5b33ca015af6c17ef5291c428abca632a09a
Divyanshusingh152 <169074483+Divyanshusingh152@users.noreply.github.com>
2025-03-12 12:33:46 +0530

    Added SQL queries for Ola Data Analysis

---
+++ queries.sql
@@ -0,0 +1,19 @@
+SELECT * FROM ola.avg_cus;
+
+SELECT * FROM ola.incom_ride;
+
+SELECT * FROM ola.max_min_driver_rate;
+
+SELECT * FROM ola.no_of_cancelled_by_cum;
+
+SELECT * FROM ola.number_of_cancelled_rides_by_customers;
+
+SELECT * FROM ola.number_of_rides_cancelled_by_drivers;
+
+SELECT * FROM ola.ride_distance_for_each_vehicle;
+
+SELECT * FROM ola.success_bookings;
+
+SELECT * FROM ola.t_b_r_c_s;
+
+SELECT * FROM ola.top_5_customers;
